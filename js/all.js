879ff94db89fb3ce24322cd0f2fa82d59bec0c1b
GLaDOLCE <38886701+GLaDOLCE@users.noreply.github.com>
2018-07-21 17:52:29 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
     $(".aboutus1").click(function (e) { 
         e.preventDefault();
-        $(".aboutus1").toggleClass("active");
+        $(".home1").toggleClass("active");
+        $(".home2,.home3").removeClass('active');
         $(".scroll1").slideToggle(500);
+        $(".scroll2,.scroll3").slideUp(500);
     });
     $(".aboutus2").click(function (e) { 
         e.preventDefault();
-        $(".aboutus2").toggleClass("active");
+        $(".home2").toggleClass("active");
+        $(".home1,.home3").removeClass('active');
         $(".scroll2").slideToggle(500);
+        $(".scroll1,.scroll3").slideUp(500);
     });
     $(".aboutus3").click(function (e) { 
         e.preventDefault();
-        $(".aboutus3").toggleClass("active");
+        $(".home3").toggleClass("active");
+        $(".home1,.home2").removeClass('active');
         $(".scroll3").slideToggle(500);
+        $(".scroll1,.scroll2").slideUp(500);
+        
     });
 });
